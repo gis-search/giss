e906c9b269bacf672e48984ca64f7cff6016da7a
gis-search <gis.search.2017@gmail.com>
2017-03-19 21:48:19 +0300

    Introduce tests

---
+++ giss-core/src/test/java/ru/giss/util/StringUtilTest.java
@@ -0,0 +1,27 @@
+package ru.giss.util;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class StringUtilTest {
+    @Test
+    public void biGrams() {
+        String[] biGrams = StringUtil.nGrams(2, "Москва");
+
+        assertArrayEquals(
+                new String[]{"Мо", "ос", "ск", "кв", "ва"},
+                biGrams
+        );
+    }
+
+    @Test
+    public void triGrams() {
+        String[] triGrams = StringUtil.nGrams(3, "Москва");
+
+        assertArrayEquals(
+                new String[]{"Мос", "оск", "скв", "ква"},
+                triGrams
+        );
+    }
+}
