f42c5387211705091f292630e126b18fb78ea60b
gis-search <gis.search.2017@gmail.com>
2017-04-12 22:14:15 +0300

    Move components to Spring rails

--- giss-api/src/main/java/ru/giss/web/controller/ApiController.java
+++ giss-api/src/main/java/ru/giss/web/controller/ApiController.java
@@ -1,27 +1,21 @@
 package ru.giss.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import ru.giss.app.BackendBuilder;
 import ru.giss.model.Address;
 import ru.giss.search.Match;
 import ru.giss.search.SearchRequest;
 import ru.giss.search.Searcher;
 import ru.giss.web.dto.SearchResult;
 
-import java.io.IOException;
 import java.util.ArrayList;
 
 @RestController
 public class ApiController {
-
-    // TODO inject properly
-    private final Searcher citySearcher;
-
-    public ApiController() throws IOException {
-        citySearcher = new BackendBuilder().build().getCitySearcher();
-    }
+    @Autowired
+    private Searcher citySearcher;
 
     @GetMapping("/search")
     public SearchResult search(
