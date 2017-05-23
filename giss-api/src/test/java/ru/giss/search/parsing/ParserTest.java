865c48ce8b71cb5a59d00a40e531d88668436606
gis-search <gis.search.2017@gmail.com>
2017-05-23 17:29:24 +0300

    Test rails

---
+++ giss-api/src/test/java/ru/giss/search/parsing/ParserTest.java
@@ -0,0 +1,28 @@
+package ru.giss.search.parsing;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import ru.giss.config.RootConfig;
+
+import java.util.List;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = RootConfig.class)
+public class ParserTest {
+    @Autowired
+    private Parser parser;
+
+    @Test
+    public void test() {
+        List<ParseResult> parseResults = parser.parse("сакнт питербург ленена");
+
+        ParseResult mostRelevant = parseResults.get(0);
+
+        Assert.assertTrue(mostRelevant.getAddress().fullName().contains("Санкт-Петербург"));
+        Assert.assertTrue(mostRelevant.getAddress().fullName().contains("Ленина"));
+    }
+}
